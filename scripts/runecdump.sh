3beb312aea2201bf1a42aa158897373f488f7192
Russ Tremain <russt@releasetools.org>
2013-03-14 16:20:08 -0700

    added showtimes support Committed on Thu Mar 14 16:20:08 PDT 2013

--- scripts/runecdump.sh
+++ scripts/runecdump.sh
@@ -3,21 +3,26 @@
 
 p=`basename $0`
 
-#make sure PROJECT dir is in path:
-PATH="${PROJECT}:$PATH"
-
 yymmddhhmm=`date +%y%m%d%H%M`
 #echo $yymmddhhmm
 
-export ECDUMPROOT LOGROOT
-ECDUMPROOT="$PROJECT/$yymmddhhmm"
-LOGROOT="$PROJECT/logs"
+export ECDUMPROOT LOGROOT BUILDTIME_LOG
+ECDUMPROOT=$PROJECT/$yymmddhhmm
+LOGROOT=$PROJECT/logs/$yymmddhhmm
+BUILDTIME_LOG=$LOGROOT/bldtime.log
 
-logfile="$LOGROOT/$yymmddhhmm".log
+logfile=$LOGROOT/ecdump.log
 
-mkdir -p "$LOGROOT"
+mkdir -p $LOGROOT
 echo logfile is $logfile
 
 ##### run the command:
-doecdump.sh > "$logfile" 2>&1 
-exit $?
+bldmsg -p $p -markbeg doecdump
+doecdump.sh > $logfile 2>&1 
+status=$?
+bldmsg -p $p -status $status -markend doecdump
+
+cd $LOGROOT
+showtimes
+
+exit $status
