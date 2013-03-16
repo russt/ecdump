a9be65c333270b789e4b4857636e93cbd7390124
Russ Tremain <russt@releasetools.org>
2013-03-15 17:20:37 -0700

    added step to remove new dump if it is identical to last Committed on Fri Mar 15 17:20:37 PDT 2013

--- scripts/doecdump.sh
+++ scripts/doecdump.sh
@@ -1,6 +1,46 @@
 #!/bin/sh
 #doecdump.sh - run ecdump.
 
+remove_if_nodiffs()
+#Usage:  remove_if_nodiffs lastdumpdir newdumpdir
+{
+    #see if we can delete the new dump.  we are looking for a ddiff file that
+    #looks like this:
+    #+-----------------------------------------------------------------+
+    #|### In 1303151230 but not in 1303151330:                         |
+    #|    NULL                                                         |
+    #|### In 1303151330 but not in 1303151230:                         |
+    #|    NULL                                                         |
+    #|### In both 1303151230 and 1303151330:, with different content:  |
+    #|    NULL                                                         |
+    #+-----------------------------------------------------------------+
+
+    #get last, curr run dirs:
+    lastrun="$1"
+    newrun="$2"
+
+    echo remove_if_nodiffs: lastrun=$lastrun newrun=$newrun
+
+    #note - first char is actually a tab, but grep does not allow \t.  RT 3/15/13
+    nullcnt=`grep '^.NULL$' logs/$newrun/ddiff.log | wc -w`
+    _rind_status=0
+
+    if [ "$nullcnt" -eq 3 ]; then
+        bldmsg -p $p -mark New run has no changes, removing it: $newrun
+        bldmsg -p $p -markbeg remove identical dump
+        rm -rf "$newrun"
+        _rind_status=$?
+        bldmsg -p $p -status $_rind_status -markend remove identical dump
+        if [ $_rind_status -ne  0 ]; then
+            bldmsg -p $p -error -status $_rind_status command failed:  rm -rf "$newrun"
+        fi
+    else
+        bldmsg -p $p -mark New run has changes - see logs/$newrun/ddiff.log for differences
+    fi
+
+    return $_rind_status
+}
+
 p=`basename $0`
 if [ "$ECDUMPROOT" = "" ]; then
     bldmsg -p $p -error please set ECDUMPROOT to the dump directory
@@ -31,7 +71,9 @@ fi
 ######
 
 #find the last 2 dumps:
-LAST2RUNS=`\ls -1 | grep -v '[^0-9]' | tail -2`
+# yymmddhhmm
+#                             yy        mm        dd        hh        mm
+LAST2RUNS=`\ls -1 | grep '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' | tail -2`
 nfiles=`echo $LAST2RUNS | wc -w`
 
 if [ $nfiles -eq 2 ]; then
@@ -43,4 +85,5 @@ else
     exit 1
 fi
 
-exit 0
+remove_if_nodiffs $LAST2RUNS
+exit $?
