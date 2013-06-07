eef652c63137dc4ce53f741b3136735b3e40aed4
Russ Tremain <russt@releasetools.org>
2013-06-07 16:55:23 -0700

    change license to bsd 2 clause Committed on Fri Jun  7 16:55:23 PDT 2013

--- ecdump.pl
+++ ecdump.pl
@@ -1,29 +1,34 @@
 #
 # BEGIN_HEADER - DO NOT EDIT
-#
-# The contents of this file are subject to the terms
-# of the Common Development and Distribution License
-# (the "License").  You may not use this file except
-# in compliance with the License.
-#
-# You can obtain a copy of the license at
-# https://open-esb.dev.java.net/public/CDDLv1.0.html.
-# See the License for the specific language governing
-# permissions and limitations under the License.
-#
-# When distributing Covered Code, include this CDDL
-# HEADER in each file and include the License file at
-# https://open-esb.dev.java.net/public/CDDLv1.0.html.
-# If applicable add the following below this CDDL HEADER,
-# with the fields enclosed by brackets "[]" replaced with
-# your own identifying information: Portions Copyright
-# [year] [name of copyright owner]
-#
-
-#
 # @(#)ecdump.pl
-# Copyright 2013-2013 Russ Tremain. All Rights Reserved.
-#
+# 
+# Author:  Russ Tremain
+# 
+# Copyright (c) 2013-2013 Perforce Software, Inc.. All Rights Reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PERFORCE 
+# SOFTWARE, INC. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+# DAMAGE.
+# 
 # END_HEADER - DO NOT EDIT
 #
 
